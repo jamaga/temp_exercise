fc1e8a1ee77ca96b9e190bb7dab2f8fe21449fcd
jamaga <hipo.beaver@gmail.com>
2015-04-17 13:13:22 +0200

    versja lepsza

--- temperatury.rb
+++ temperatury.rb
@@ -1,71 +1,31 @@
-choiceList = %w{FA CE KE}
-currentTempChoice = 'FA'
+class Temperatura
+  def initialize(temp = 0, system = :k)
 
-puts "temperature calculator"
+    case system
+      when :c
 
-begin
-
-  begin
-
-    print  "\nTemperature choices, please enter the entire word of at least two letters
-						\n ~(Ce)lsius, \n ~(Fa)hrenheit, \n ~(Ke)lvin
-							\nPlease enter temperture TYPE you want to convert (From the list above): "
-
-
-    if ( (choiceList.include? currentTempChoice) == false)  && currentTempChoice != 'Q'
-      print "\nInvalid temperature choice, please try again"
-    end
-
-    currentTempChoice = gets.upcase
-    currentTempChoice = currentTempChoice[0] + currentTempChoice[1]
-
-  end while ( (choiceList.include? currentTempChoice) == false) && currentTempChoice != 'Q'
-
-
-  unless currentTempChoice[0] == "Q"
-
-    print "\nTo which temperature KIND you want to convert your temp: "
-    newTempChoice = gets.upcase
-
-    newTempChoice = newTempChoice[0] + newTempChoice[1]
-
-    print "\nPlease, enter the value of temp you want to convert: "
-    temperature = gets.chomp.to_f
-
-    case currentTempChoice
-
-      when 'CE' #celsius to fahrenheit
-        convertedTemp = temperature * 9/5 + 32
-
-      when 'KE'#Kelvin to fahrenheit
-        convertedTemp = temperature * 9/5 - 459.67
+      when :f
 
       else
-        convertedTemp = temperature
+        @t = temp
 
     end
-
-
-    case newTempChoice
-
-      when 'CE' #fahrenheit to celsius
-        newConName = "celsius"
-        finalTemp = (convertedTemp - 32) * 5/9
-
-      when 'KE'#far to Kelvin
-        newConName = "Kelvin"
-        finalTemp = (convertedTemp + 459.67) * 5/9
-
-      when "FA"
-        newConName = "Fahrenheit"
-        finalTemp = convertedTemp
-
-    end
-
-    puts "Your temperature in #{newConName} is #{finalTemp.to_f.round(2)}"
-    print "Continute(type quit to stop, or anything to continue)? :"
-    redoing = gets.upcase
 
   end
 
-end while currentTempChoice[0] != "Q" && redoing[0] != "Q"
+  def in_c
+
+  end
+
+  def in_f
+
+  end
+
+  def in_k
+    @t
+  end
+
+end
+
+#testy sie 4x mniej wiecej
+#to samo c i f
---
+++ testy_temperatury.rb
@@ -0,0 +1,48 @@
+require 'minitest/autorun'
+require './temperatury'
+
+class TemperaturyTest < Minitest::Test
+
+  def test_kelvin_fail
+    t = Temperatura.new
+    assert_not_equal -272.15, t.in_k
+  end
+
+  def test_kelvin_success
+    t = Temperatura.new
+    assert_equal 0, t.in_k
+  end
+
+  def test_celc_fail
+    t = Temperatura.new
+    assert_not_equal 0, t.in_c
+  end
+
+  def test_celc_success
+    t = Temperatura.new
+    assert_equal -272.15, t.in_c
+  end
+
+  def test_far_fail
+    t = Temperatura.new
+    assert_not_equal 0, t.in_f
+  end
+
+  def test_far_success
+    t = Temperatura.new
+    assert_equal -457.87, t.in_f
+  end
+
+end
+
+
+
+t1 = Temperatura.new(100, :c)
+t1.in_c  #=> 100
+t1.in_k  #=> 373.15
+t1.in_f  #=> 212
+
+t2 = Temperatura.new
+t2.in_c  #=> -272.15
+t2.in_k  #=> 0
+t2.in_f  #=> -457.87
