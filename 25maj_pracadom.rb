e44a86571068b7615f3615339687423b0c9e0fc6
jamaga <hipo.beaver@gmail.com>
2015-05-25 15:00:53 +0200

    working add function

---
+++ 25maj_pracadom.rb
@@ -0,0 +1,67 @@
+class Base
+
+  attr_accessor :added_name, :added_email
+
+  def initialize
+    @baza = []
+  end
+
+  def add(hash)
+    @baza.push({ 'name' => hash['name'], 'email' => hash['email']})
+  end
+  #
+  # def get_email(added_name)
+  #   @baza[added_name]
+  # end
+  #
+  # def delete(added_name)
+  #   @baza.reject! { |h| h['name'] == added_name}
+  # end
+  #
+  # def get_all
+  #   @baza
+  # end
+  #
+  # def purge
+  #   @baza = []
+  # end
+
+end
+
+
+o = Base.new
+p o
+o.add({ 'name' => 'Piotr', 'email' => 'example@main.com' })
+p o
+# o.get_email('Piotr') # => example@main.com
+# o.add {'name' => 'Ala', 'email' => 'ala@main.com'}
+# o.get_email('Ala') # => ala@main.com
+# o.delete('Piotr')
+# o.get_email('Piotr') # => nil
+# o.get_all # => [  {name: ‘Ala’, email: ‘ala@main.com ’} ]
+# o.purge
+# o.get_all # => []
+
+
+
+# napisanie klasy ktora przyjmuje adres email i imie
+#
+# symulacja bazy danych - wywolujemy nowa instancje klasy base
+# mozemy dodawac hash do bazy - name i email
+# jak wywolamy get_email  - zwraca email
+# jak nie ma maila - zwraca email
+# mozna kasowac dane
+# get_all - cala baza
+# mozna wyczyscic cala tablice - purge
+# najpierw testy!
+#
+# o = new Base
+# o.add {name: ‘Piotr’, email: ‘example@main.com ’}
+# o.get_email(‘Piotr’) # => example@main.com
+# o.add {name: ‘Ala’, email: ‘ala@main.com ’}
+# o.get_email(‘Ala’) # => ala@main.com
+# o.delete(‘Piotr’)
+# o.get_email(‘Piotr’) # => nil
+# o.get_all # => [  {name: ‘Ala’, email: ‘ala@main.com ’} ]
+# o.purge
+# o.get_all # => []
