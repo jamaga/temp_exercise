ddfde2ca71922686010ca790b65fd9c63e5843d9
jamaga <hipo.beaver@gmail.com>
2015-05-28 15:14:04 +0200

    solved ex1 from hmork

--- 28maj_debug.rb
+++ 28maj_debug.rb
@@ -11,18 +11,24 @@ module App
     def email
       @item[:email]
     end
+
+    def id
+      @item[:id]
+    end
   end
 
-  class List
+  class List < App::Item
     def initialize
       @data = []
     end
 
     def add(item)
-      @data << Item.new(item)
+      @data << App::Item.new(item)
+      #p @data
     end
 
     def get_email(name)
+      p @data
       @data.detect { |item| item.name == name }.email
     end
 
@@ -32,13 +38,11 @@ module App
   end
 end
 
-
-
-list = App.List.new
-list.add({ "id" => 1, "name" => "Alice", "email" => "alice@domain.com" })
-list.add({ "id" => 2, "name" => "Agnes", "email" => "agnes@domain.com" })
-list.add({ "id" => 3, "name" => "Peter", "email" => "peter@domain.com" })
-list.add({ "id" => 4, "name" => "Mike", "email" => "mike@domain.com" })
+list = App::List.new
+list.add({ id: 1, name: "Alice", email: "alice@domain.com" })
+list.add({ id: 2, name: "Agnes", email: "agnes@domain.com" })
+list.add({ id: 3, name: "Peter", email: "peter@domain.com" })
+list.add({ id: 4, name:  "Mike", email: "mike@domain.com" })
 
 puts list.get_email('Agnes')  # => 'agnes@domain.com'
 puts list.get_id('Peter')     # => 3
