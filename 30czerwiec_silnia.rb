9ede3cd6e7ba37fd4d5733998f2f116b64bcb958
jamaga <hipo.beaver@gmail.com>
2015-06-30 12:06:24 +0200

    done exercise tuesday

---
+++ 30czerwiec_silnia.rb
@@ -0,0 +1,31 @@
+# 5!
+#  1  2  3  4  5
+#  0! = 1
+
+def silnia(number)
+  result = 1
+  while number > 1
+    result = result*number
+    number -= 1
+  end
+  result
+end
+
+p silnia(5)
+p "--------------------"
+
+def silnia2(number)
+  result = 1
+    for x in 1..number
+      result = result*x
+    end
+  result
+end
+
+p silnia2(5)
+
+
+  # for i in 1..8 do
+  #   puts i
+  # end
+
---
+++ 30czerwiec_suma.rb
@@ -0,0 +1,106 @@
+# sumowanie 1,6,2,1
+
+def adding(array)
+  result = 0
+  for x in array
+    result += x
+  end
+  result
+end
+
+p adding([1,5,2,1,4,6])
+
+def adding_even(array)
+  result = 0
+  for x in array
+    if x%2 == 0
+      result += x
+    end
+  end
+  result
+end
+
+p adding_even([1,5,2,1,4,6])
+
+
+def add_full_num(array)
+  result = 0
+  for x in array
+    if x%10 == 0
+      result += x
+    end
+  end
+  result
+end
+
+p add_full_num([1,2,5,45, 10, 20, 5, 6, 40])
+
+#przyjmuje hasz - x = {“a” => 1, “b” => [1,2,3], “c” => “ala ma kota” }
+#zsumuje wszystie liczby w haszu
+# value.kind_of?(Array)
+
+def sum_numbers(h)
+  result = 0
+  h.each do |key, value|
+    if value.kind_of?(Integer)
+      result += value
+    elsif value.kind_of?(Array)
+      for x in value
+        result += x
+      end
+    end
+  end
+  result
+end
+
+p sum_numbers({"a" => 1, "b" => [1,2,3], "c" => "ala ma kota" })
+
+#funkcja ktora mnozy wszystkie elementy tablicy
+
+def multiply_from_array(array)
+  result = 1
+  for x in array
+    result *= x
+  end
+  result
+end
+
+p multiply_from_array([2,5])
+
+#DODA2QC  co drugie element [1,3,5,6,7] = 3 + 6
+
+def add_every_second(array)
+  result = 0
+  n = 0
+  for x in array
+    if n % 2 == 0
+      result += x
+    end
+    n += 1
+  end
+  result
+end
+
+p add_every_second([1,3,5,6,7])
+
+#hasz - przyjmuje, hasz jak wyzej
+# program ma mnozyc liczby w tablicach
+
+# x = {“a” => 1, “b” => [1,2,3], “c” => “ala ma kota”, “d” => [2,4,6] }   , 288
+
+def multiply_in_array(hash)
+  result = 1
+  hash.each do |key, value|
+    if value.kind_of?(Array)
+      for x in value
+        result *= x
+      end
+    end
+  end
+  result
+end
+
+p multiply_in_array({"a" => 1, "b" => [1,2,3], "c" => "ala ma kota", "d" => [2,4,6] })
+
+
+
