cd4644561ba9e7e1551e8819565a541d1cb51b80
jamaga <hipo.beaver@gmail.com>
2015-07-07 17:57:50 +0200

    passing test for my each with index

--- 16czerwiec_cw_enumberable_tests.rb
+++ 16czerwiec_cw_enumberable_tests.rb
@@ -13,5 +13,18 @@ class MyenumerableTest < Test::Unit::TestCase
     end
     assert_equal pusta, [2, 5, 7, 4]
   end
+
+  def test_my_each_with_index
+    pusta = []
+    [2, 5, 7, 4].my_each_with_index do |element|
+      pusta << element
+    end
+    assert_equal pusta, [2, 5, 7, 4]
+  end
+
+  def test_my_select
+    
+  end
+
 end
 
