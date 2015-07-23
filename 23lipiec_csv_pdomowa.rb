1962d82afa3800717b806813aa3a8b65f67be414
jamaga <hipo.beaver@gmail.com>
2015-07-23 16:19:57 +0200

    thursday afternoon work

---
+++ 23lipiec_csv_pdomowa.rb
@@ -0,0 +1,36 @@
+module App
+
+  class Index
+
+    #jest kolekcja obiektow typu Person
+    def add(name_to_add, surname_to_add, age_to_add )
+      CSV.open("./23lipiec_csv_pdomowa.csv", "wb") do |csv|
+       csv << [name_to_add, surname_to_add, age_to_add]
+      end
+    end
+
+    def remove(person_row)
+      source = File::open("./23lipiec_csv_pdomowa.csv","r")
+      source.each_line do |line, i|
+        if i == person_row
+
+        end
+      end
+
+    end
+  end
+
+  class Person
+    attr_accessor :person_name, :person_surname, :person_age
+  end
+
+end
+
+#CSV.open('./23lipiec_csv_pdomowa.csv')
+
+# writing:
+#     CSV.open("./23lipiec_csv_pdomowa.csv", "wb") do |csv|
+#       csv << ["row", "of", "CSV", "data"]
+#       csv << ["another", "row"]
+#       # ...
+#     end
---
+++ 23lipiec_csv_pdomowa_tests.rb
@@ -0,0 +1,10 @@
+require 'test/unit'
+require './23lipiec_csv_pdomowa'
+
+class AppTest < Test::Unit::TestCase
+
+  def test_adding
+
+  end
+
+end
