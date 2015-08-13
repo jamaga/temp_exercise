90fd4c0816c4aa04d94134b594574ad120b51fa9
jamaga <hipo.beaver@gmail.com>
2015-08-13 13:06:58 +0200

    fight with details showing

--- 13sierpien_lekcja.rb
+++ 13sierpien_lekcja.rb
@@ -12,24 +12,43 @@ module UserApp
 
     def add(user_hash, user_details_hash)
       User.add(@db, user_hash)
-      UserDetails.add(user_details_hash)
+      UserDetails.add(@db, user_details_hash)
     end
 
-    # def details(user_username)
-    #
+    def details(user_username)
+      result = @db.query("select * from users where username = '#{user_username}'").first
+      p result
+      id_we_look_for = result['id']
+      p id_we_look_for
+
+      status_we_look_for = result['status']
+      p status_we_look_for
+
+      email_we_look_for = result['email']
+      p email_we_look_for
+
+      result_from_user_details = @db.query("select * from user_details where id = '#{id_we_look_for}'").first
+      p result_from_user_details
+    end
+
+
+    # def show(fruit_id_to_show)
+    #   results = @db.query("select * from products where id = '#{fruit_id_to_show}'").first
+    #   #p @db.query("select * from products where id = '#{fruit_id_to_show}'")
+    #   if results != nil
+    #     results["name"] + ': ' + results["price"].to_s
+    #   else
+    #     'Product does not exist!'
+    #   end
     # end
-    #
+
     # def drop(user_username)
     #   @db.query("delete from users where username = '#{user_username}'")
     # end
     #
-    # def list
-    #   list_array = []
-    #   @db.query("select * from users").each do |rez|
-    #     list_array << rez[:login]
-    #   end
-    #   list_array
-    # end
+    def list
+      @db.query("select * from users").map { |one_h| one_h['username'] }
+    end
   end
 
   class User
@@ -72,16 +91,15 @@ module UserApp
 
   class UserDetails
 
-    def add
-      username_to_add = user_hash[:username]
-      email_to_add = user_hash[:email]
-      status_to_add = user_hash[:status]
-      @db.query("insert into users (username, email, status) values ('#{username_to_add}',
-                                                                        '#{email_to_add}',
-                                                                        '#{status_to_add}')")
+    def self.add(db_connection, second_table_data)
+      first_name_to_add = second_table_data[:first_name]
+      last_name_to_add = second_table_data[:last_name]
+      db_connection.query("insert into user_details (first_name, last_name) values ('#{first_name_to_add}',
+                                                                        '#{last_name_to_add}')")
     end
-    #dane z tabeli user_details
 
+
+    #dane z tabeli user_details
     #rozszezenie klasy user
     #druga tabela
 
