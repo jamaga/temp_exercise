dbc4ee6b41571afafe0f0af02cddbf4cbb7c9d0e
jamaga <hipo.beaver@gmail.com>
2015-08-25 12:06:20 +0200

    wrote tests for the todolistApp application

---
+++ 25sierpien_pdomowa.rb
@@ -0,0 +1,12 @@
+module TodoListApp
+
+  class ListItem
+    #tutaj piszemy sqlki
+  end
+
+  class List < ListItem
+    #tutaj mamy metody ale bez squelek
+
+  end
+
+end
---
+++ 25sierpien_pdomowa_testy.rb
@@ -0,0 +1,72 @@
+require 'test/unit'
+require './25sierpien_pdomowa'
+require 'mysql2'
+
+class TodoListAppTest < Test::Unit::TestCase
+
+  def initialize(test_case_class)
+    super
+    @db = Mysql2::Client.new(host: "localhost", username: "root",
+        password: "", database: "ruby_tests")
+  end
+
+  def setup
+    sql = '
+      CREATE TABLE IF NOT EXISTS `todo_lists`(
+        `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+        `name` VARCHAR(255) NOT NULL,
+        `description` TEXT,
+        `is_active` BOOL NOT NULL DEFAULT true,
+        PRIMARY KEY(ID)
+      ) engine=InnoDB;'
+
+    @db.query(sql)
+
+    @list = TodoListApp::Shop.new(@db)
+  end
+
+  def teardown
+    @db.query('DROP TABLE `todo_lists`')
+  end
+
+  def test_add
+
+  end
+
+  def test_add_item
+    assert_equal 1, @list.add('shop', 'buy fruit', true)
+    assert_equal 2, @list.add('pharmacy', 'buy medicine', true)
+  end
+
+  def test_list_product
+    @list.add('shop', 'buy fruit', true)
+    @list.add('pharmacy', 'buy medicine', true)
+    @list.add('warsztat', 'zrobic przeglad', false)
+    assert_equal [Hash[ :id => 1, :name => 'shop'],
+                  Hash[ :id => 2, :name => 'pharmacy']], @list.list
+  end
+
+  def test_drop_product
+    @list.add('shop', 'buy fruit', true)
+    @list.add('pharmacy', 'buy medicine', true)
+    assert @shop.drop(1)
+    assert_equal [Hash[ :id => 2, :name => 'pharmacy']], @list.list
+  end
+
+  def test_show_product
+    @list.add('shop', 'buy fruit', true)
+    @list.add('pharmacy', 'buy medicine', true)
+    assert_equal Hash[ :id => 2, :name => 'pharmacy', :description => 'buy medicine', :is_active => 1 ], @list.details(2)
+  end
+
+  def test_change_status
+    @list.add('shop', 'buy fruit', true)
+    @list.add('pharmacy', 'buy medicine', true)
+    @list.add('warsztat', 'zrobic przeglad', false)
+    assert @list.change_state(3, true)
+    assert_equal [Hash[ :id => 1, :name => 'shop'],
+                  Hash[ :id => 2, :name => 'pharmacy'],
+                  Hash[ :id => 3, :name => 'warsztat']], @list.list
+  end
+
+end
