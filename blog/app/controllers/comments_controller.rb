a7a16810ff4b12e1929d971008e5627c25b2f799
jamaga <hipo.beaver@gmail.com>
2015-09-01 08:37:14 +0200

    tuesday rails exercises

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -8,6 +8,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create!(params[:comment])
-    redirect_to @post
+    #redirect_to @post
+    redirect_to post_path(@post.id)
   end
 end
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,9 +1,12 @@
 Blog::Application.routes.draw do
 
+  get "statics/contact"
+
   #komentzrze sa zalezne od postow
   #i jest tylko akcja create komentarzy - chcemy tylko tworzy ckomentarze do postw
   resources :posts do
     resources :comments, :only => [:create]
   end
 
+  root :to => "statics#home"
 end
