662a77837230c11da81f328bfc7b0d537693d9ca
jamaga <hipo.beaver@gmail.com>
2015-10-22 15:07:56 +0200

    did tests for validations

--- blog_rails4/test/models/contact_test.rb
+++ blog_rails4/test/models/contact_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class ContactTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'body is mandatory' do
+    assert_equal Contact.new.errors[:body].any?, false
+    assert_equal Contact.create.errors[:body].any?, true
+  end
+
+  test 'author is mandatory' do
+    assert_equal Contact.new.errors[:author].any?, false
+    assert_equal Contact.create.errors[:author].any?, true
+  end
+
+  test 'subject is mandatory' do
+    assert_equal Contact.new.errors[:subject].any?, false
+    assert_equal Contact.create.errors[:subject].any?, true
+  end
+
+  test 'valid when have all attributes' do
+    contact = Contact.new(author: 'oskar', body: 'lalalalala', subject: 'pancerny i pies')
+    assert_equal contact.valid?, true
+
+    assert_equal contact.author, 'oskar'
+    assert_equal contact.body, 'lalalalala'
+    assert_equal contact.subject, 'pancerny i pies'
+  end
+
+  test 'not valid when attribute is missing' do
+    contact = Contact.new(author: 'oskar', body: 'lalalalala', subject: nil)
+    assert_equal contact.valid?, false
+  end
 end
