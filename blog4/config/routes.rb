94b94106e74a6bc392d8fb8276da3b02dbb55696
jamaga <hipo.beaver@gmail.com>
2015-10-28 13:22:21 +0100

    bootstrap2

--- blog4/config/routes.rb
+++ blog4/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  get "registrations/new"
+  get 'registrations/new'
+
+  match 'users/sign_in', via: [:get, :post]
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
