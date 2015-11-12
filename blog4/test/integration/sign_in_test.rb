9693c1e67bccd7cad84e0c5e65e830fff85ba780
jamaga <hipo.beaver@gmail.com>
2015-11-12 12:49:40 +0100

    working sign in test success and failure

--- blog4/test/integration/sign_in_test.rb
+++ blog4/test/integration/sign_in_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class SignInTest < ActionDispatch::IntegrationTest
-  test 'successful login' do
+  test 'successful sign in' do
     get '/users/sign_in'
     assert_response :success
     assert_select 'form' do
@@ -12,11 +12,22 @@ class SignInTest < ActionDispatch::IntegrationTest
     post_via_redirect '/users/sign_in', :user => { :email => 'john@domain.com', :password => 'qwerty12!@' }
     assert_equal '/users/sign_in', path
     assert_select '.alert-info', 'Signed in successfully.'
+  end
 
-    #moge sie zalogowac jak uzytkownik istnieje
-    #ma byc ten z yamla - stworzny przez fixtury -  ma byc john
-    #i ze sie nie udalo zalogowac - drugi test
+  #moge sie zalogowac jak uzytkownik istnieje
+  #ma byc ten z yamla - stworzny przez fixtury -  ma byc john
+  #i ze sie nie udalo zalogowac - drugi test
 
+  test 'unsuccessful sign in' do
+    get '/users/sign_in'
+    assert_response :success
 
+    assert_select 'form' do
+      assert_select '[name=?]', 'user[email]'
+      assert_select '[name=?]', 'user[password]'
+    end
+
+    post_via_redirect '/users/sign_in', :user => { :email => 'john@op.pl', :password => 'lalalala' }
+    assert_select '.alert-warning', 'Invalid email or password.'
   end
 end
