054ee7e6342db2159dfcbc53c962577782240ec8
jamaga <hipo.beaver@gmail.com>
2015-11-12 12:37:14 +0100

    working successful login test

--- blog4/test/integration/sign_in_test.rb
+++ blog4/test/integration/sign_in_test.rb
@@ -5,20 +5,18 @@ class SignInTest < ActionDispatch::IntegrationTest
     get '/users/sign_in'
     assert_response :success
     assert_select 'form' do
-      assert_select '[name=?]', 'log_in[email]'
-      assert_select '[name=?]', 'log_in[password]'
+      assert_select '[name=?]', 'user[email]'
+      assert_select '[name=?]', 'user[password]'
     end
 
-    #tutaj ma byc post ??
-    post_via_redirect '/log_in/save', :log_in => { :email => 'john@op.pl', :password => 'qwertyui' }
-    assert_equal '/log_in/success', path
+    post_via_redirect '/users/sign_in', :user => { :email => 'john@domain.com', :password => 'qwerty12!@' }
+    assert_equal '/users/sign_in', path
+    assert_select '.alert-info', 'Signed in successfully.'
 
-    #po zalogowaniu ma byc - komunikat - jak zalogujemy sie jesli jest komunikat to tak
-    #urle popawic
     #moge sie zalogowac jak uzytkownik istnieje
     #ma byc ten z yamla - stworzny przez fixtury -  ma byc john
-
     #i ze sie nie udalo zalogowac - drugi test
+
 
   end
 end
