678b572765c838c1124d206d742603702a9cfde1
jamaga <hipo.beaver@gmail.com>
2015-11-16 18:13:20 +0100

    dsa

--- blog4/test/integration/forgot_password_test.rb
+++ blog4/test/integration/forgot_password_test.rb
@@ -11,7 +11,18 @@ class ForgotPasswordTest < ActionDispatch::IntegrationTest
     assert_response :success
     # assert_redirected_to '/users/password/new'
     #dlaczego nie dziala ?
-    assert_select 'input.btn.btn-info', 'Send me reset password instructions.'
+    assert_select '.input.btn.btn-info', 'Send me reset password instructions.'
 
+  end
+
+  test 'field with password lefft blank' do
+    get '/users/sign_in'
+    assert_response :success
+    assert_select '.btn-link', 'Forgot your password?'
+
+    get '/users/password/new'
+    assert_response :success
+    post_via_redirect '/users/password', :user => { :email => '' }
+    assert_select '.alert-danger li', 'Email can\'t be blank'
   end
 end
