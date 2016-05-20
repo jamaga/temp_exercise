63cdd0bad2b5fccc4ed76f8028a6a601d24e8578
jamaga <hipo.beaver@gmail.com>
2016-05-20 13:14:12 +0200

    cd

--- kalkulator2.rb
+++ kalkulator2.rb
@@ -19,49 +19,72 @@
 
 require 'test/unit'
 
-class KaklulatorDwa
+class KalkulatorDwa
 
-  File.read('kalkulator2.txt').split("\n").each do |line|
-    wynik = 0
-    linijka = line.split('|')
-
-    if linijka[0] == 'dodaj'
-      licz_w_arr = linijka[1].split(',')
-      licz_w_arr.each do |jedna_liczba|
-        wynik = wynik + jedna_liczba.to_i
-      end
-      p wynik
-
-    elsif linijka[0] == 'odejmij'
-      licz_w_arr = linijka[1].split(',')
-      licz_w_arr.each do |jedna_liczba|
-        licz_w_arr[0]
-        wynik = wynik - jedna_liczba.to_i
-      end
-      p wynik
-    else
-      raise 'nie zdefiniowane dzialanie!!!'
+  def wykonaj_dzialanie(jedna_linijka)
+    tablica_z_linijki = jedna_linijka.split('|')
+    case tablica_z_linijki.first
+      when 'dodaj'
+        dodaj(tablica_z_linijki[1])
+      when 'odejmij'
+        odejmij(tablica_z_linijki[1])
+      else
+        raise 'NIE MA takiego dzialania!!!!'
     end
   end
+
+  def dodaj(tekst_z_liczbami)
+    wynik = 0
+    tekst_z_liczbami.split(',').each do |jedna_liczba|
+      wynik = wynik + jedna_liczba.to_f
+    end
+    wynik
+  end
+
+  def odejmij(tekst_z_liczbami)
+    tablica_z_liczbami = tekst_z_liczbami.split(',')
+    wynik = tablica_z_liczbami.delete_at(0)
+    wynik = wynik.to_f
+
+    tablica_z_liczbami.each do |jedna_liczba|
+      wynik = wynik - jedna_liczba.to_f
+    end
+    wynik
+  end
+
 end
 
-
-class KalkulatorDwaTest < Test::Unit::TestCase
-
-
-  def test_dodawanie
-    k = KalkulatorDwa.new
-    assert_equal 9, k.dodaj('2,3,4')
-  end
-
-  def test_odejmowanie
-    k = KalkulatorDwa.new
-    assert_equal 2, k.odejmij('5,2,1')
-  end
-
-  def test_bez_dzialania
-    # wyguglowac
-    #assert_raise 'nie zdefiniowane dzialanie!!!'
-  end
-
+k = KalkulatorDwa.new
+File.readlines('kalkulator2.txt').each do |line|
+  p k.wykonaj_dzialanie(line.chomp)
 end
+
+# nowy plik z kalkulator_test.rb - w niego wklejam klase testowa - z require klasy kalkulator
+# dodaje nowy plik - np main_kalk.rb - w ktorym jest require klasy kalkulator i to od 57 do 60
+# obsluzyc blad - cos innego i DODAC MNOZENIE DZIELENIE- i jak chce program odpalic to main_kalk.rb a testy --> odpalam plik z testami
+
+
+# class KalkulatorDwaTest < Test::Unit::TestCase
+#
+#   def test_wykonaj_dzialanie
+#     k = KalkulatorDwa.new
+#     assert_equal 9, k.wykonaj_dzialanie('dodaj|2,3,4')
+#     assert_equal 2, k.wykonaj_dzialanie('odejmij|5,2,1')
+#   end
+#
+#   def test_dodawanie
+#     k = KalkulatorDwa.new
+#     assert_equal 9, k.dodaj('2,3,4')
+#   end
+#
+#   def test_odejmowanie
+#     k = KalkulatorDwa.new
+#     assert_equal 2, k.odejmij('5,2,1')
+#   end
+#
+#   def test_bez_dzialania
+#     # wyguglowac
+#     #assert_raise 'nie zdefiniowane dzialanie!!!'
+#   end
+#
+# end
