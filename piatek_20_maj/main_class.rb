0dd8a2fd4633ce189f9f84eb519a96fd10250a57
jamaga <hipo.beaver@gmail.com>
2016-05-24 17:47:05 +0200

    wtorek testy

--- piatek_20_maj/main_class.rb
+++ piatek_20_maj/main_class.rb
@@ -1,18 +1,53 @@
 class Main
 
+  def self.dodaj(tablica_wartosci)
+    wynik = 0
+    tablica_wartosci.map { |liczba| wynik += liczba.to_f }
+    wynik
+  end
+
+  def self.odejmij(tablica_wartosci, wynik=nil)
+    wynik = tablica_wartosci.delete_at(0) unless wynik
+    wynik = wynik.to_f
+    tablica_wartosci.each do |liczba|
+      wynik -= liczba.to_f
+    end
+    wynik
+  end
+
+  def self.mnozenie(tablica_wartosci)
+    wynik = 1
+    tablica_wartosci.each do |liczba|
+      wynik *= liczba.to_f
+    end
+    wynik
+  end
+
   def self.licz(hash)
     raise 'TO NIE JEST HASH' unless hash.kind_of?(Hash)
+
+    wynik = 0
+    typ_pierwszego_dzialania = hash.first[0]
+
     hash.each do |dzialanie, wartosci|
       raise 'TO NIE JESt TABLICA' unless wartosci.kind_of?(Array)
-      wartosci.first
-      if dzialanie == 'D'
-        wynik = 0
-        wynik = wynik = wartosci
+      case dzialanie.to_s
+        when 'D'
+          wynik += self.dodaj(wartosci)
+        when 'O'
+          if typ_pierwszego_dzialania == dzialanie
+            wynik = self.odejmij(wartosci, wartosci.first)
+          else
+            wynik -= self.odejmij(wartosci)
+          end
+        when 'M'
+          if typ_pierwszego_dzialania == dzialanie
+            wynik = 1
+          end
+          wynik *= self.mnozenie(wartosci)
       end
-      p wynik
-
-
     end
+    wynik
   end
 end
 
--- piatek_20_maj/main_test.rb
+++ piatek_20_maj/main_test.rb
@@ -4,13 +4,13 @@ require './main_class.rb'
 class MainTest < Test::Unit::TestCase
 
   def test_licz_success
-    assert_equal 21, Main.licz({
+    assert_equal -33, Main.licz({
       'D': [ 2, 3, 4 ],
       'O': [ 5, 1, 6 ],
       'M': [ 2, -1, 1.5 ]
     })
 
-    assert_equal 0, Main.licz({ })
+    #assert_equal 0, Main.licz({ })
 
     assert_equal -5, Main.licz({
       'O': [ 2, 3, 4 ]
@@ -36,4 +36,22 @@ class MainTest < Test::Unit::TestCase
     end
   end
 
+  def test_dodaj
+    assert_equal 9, Main.dodaj([ 2, '3', 4 ])
+  end
+
+  def test_odejmij
+    assert_equal 2, Main.odejmij([ 5, 0, '3' ])
+    #jesli mamy podana wart poczatkowa - wynik to wart poczatkowa
+    #jak nie - pierwsza wartosc z tablicy
+    assert_equal 1, Main.odejmij([ 5, 1, '3' ], 10)
+  end
+
+  def test_mnozenie
+    assert_equal 24, Main.mnozenie([ 2, '3', 4])
+  end
+
 end
+
+#zakomentowac asercje w jednym tescue ktory nie wypala
+# i odkomentowywac
