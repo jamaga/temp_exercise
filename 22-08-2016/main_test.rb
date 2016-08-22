af278bb3e5a3254c016ec5bcba95e8d8ce4d4b97
jamaga <hipo.beaver@gmail.com>
2016-08-22 14:51:25 +0200

    creates content in files

---
+++ 22-08-2016/main_test.rb
@@ -0,0 +1,12 @@
+require 'test/unit'
+require './main_class.rb'
+
+class MainTest < Test::Unit::TestCase
+
+  def test_one
+
+
+
+
+  end
+end
--- run.rb
+++ run.rb
@@ -22,6 +22,8 @@ class Bootstrap
 
     create_dirs
     create_files
+    check_files
+    write_in_files
   end
 
   def create_dirs
@@ -35,21 +37,42 @@ class Bootstrap
     File.new("./#{dir_name}/#{FILE_TEST_NAME}", "w+")
   end
 
-  def check_files
-    p File.exist?(@file_class_name)
-    p File.exist?(@file_test_name)
+  def write_in_files
+    File.open("./#{dir_name}/#{FILE_CLASS_NAME}", "w") do |l|
+      l.puts "class Main\n  def self.transform()\n\n  end\nend"
+    end
 
-    if File.exist?(@file_class_name) && File.exist?(@file_test_name)
+    File.open("./#{dir_name}/#{FILE_TEST_NAME}", "w") do |l|
+      l.puts "require 'test/unit'\nrequire './main_class.rb'\n\nclass MainTest < Test::Unit::TestCase\n\n  def test_one\n\n\n\n\n  end\nend"
+    end
+  end
+
+  def check_files
+    p File.exist?("./#{dir_name}/#{FILE_CLASS_NAME}")
+    p File.exist?("./#{dir_name}/#{FILE_TEST_NAME}")
+
+    if File.exist?("./#{dir_name}/#{FILE_CLASS_NAME}") && File.exist?("./#{dir_name}/#{FILE_TEST_NAME}")
       puts 'Do you want to overwrite files? - Y or N'
       answer = gets.chomp
       if answer == 'Y'
         create_files
+        write_in_files
       else
         puts 'DONE!'
         return
       end
     end
-  end
 
+    FILE_TEST_NAME.puts("write your stuff here")
+
+    File.open("#{FILE_TEST_NAME}", "w") do |f|
+      f.write('jhjjhjhjhj')
+    end
+
+  end
 end
+
 Bootstrap.new   # .new - tworzy nowa instancje - i KONSTRUKTOR SIE ODPALA
+
+
+# moze dir z numerem 1  2  3 -- bo na lekcji czasem robimy 2 albo 3 zadania i potrzebne jest wiecej
